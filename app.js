ba7caf9204ad1f6f805ee1ed28b59c6e2000ac4f
Nicholas Brouwer <82915669+nb04@users.noreply.github.com>
2024-07-16 13:27:55 -0400

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let uploadedImage;
 // Load the TensorFlow.js model
 async function loadModel() {
     try {
-        model = await tf.loadLayersModel('/tfjs-image-prediction/jsmodel_07_15/model.json'); // loadLayersModel instead of loadGraphModel
+        model = await tf.loadLayersModel('/tfjs-image-prediction/jsmodel_07_16/model.json'); // loadLayersModel instead of loadGraphModel
         console.log('Model loaded successfully');
     } catch (error) {
         console.error('Error loading model:', error);
